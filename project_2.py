deca8cae4d916e49b79984135c350d229e0dc491
eharrington2019 <32171170+eharrington2019@users.noreply.github.com>
2018-02-15 11:35:11 -0500

    started creating the fibonacci code

---
+++ project_2.py
@@ -0,0 +1,15 @@
+
+end_num = int(input("What number would you like to end at? "))
+even_odd = input("Whould youl like me to add the even, odd or all of the numbers? ")
+
+def fib():
+    a = 0
+    b = 1
+    if a == 0 or x < end_num:
+        x = a + b
+        a = b
+        b = x
+    else:
+        print(x)
+
+fib()
