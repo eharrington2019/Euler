4c24b0f797335a5e478b5dcf1a4a97a345519ee7
eharrington2019 <32171170+eharrington2019@users.noreply.github.com>
2018-02-27 11:10:19 -0500

    got the fibonacci code to work and started ot add the even and odd pieces

--- project_2.py
+++ project_2.py
@@ -5,11 +5,29 @@ even_odd = input("Whould you like me to add the even, odd or all of the numbers?
 def fib():
     a = 0
     b = 1
-    if a == 0 or x < end_num:
-        x = a + b
-        a = b
-        b = x
-    else:
-        print(x)
+    x = a + b
+    total = 0
+    for i in range(end_num)
+        while a == 0 or x < end_num:
+            a = b
+            b = x
+            x = a + b
+            if even_odd == "even":
+                total = total + x
+            elif even_odd == "odd":
+                total = total + x
+            elif even_odd == "all":
+                total = total + x
+            else:
+                print("ERROR")
+        print(total)
 
 fib()
+
+
+def is_even(x):
+    return x % 2
+
+for i in range ():
+    if(ia_even(i))
+    print(i)
