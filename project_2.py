da94d16fc60331df17cb91b6ab4e66219829e922
eharrington2019 <32171170+eharrington2019@users.noreply.github.com>
2018-03-27 11:04:32 -0400

    yjnimui

--- project_2.py
+++ project_2.py
@@ -1,33 +1,30 @@
 
-end_num = int(input("What number would you like to end at? "))
-even_odd = input("Whould you like me to add the even, odd or all of the numbers? ")
+num_fib = int(input("How many number of the fibonacci sequence would you like to add? "))
+even_odd = input("Would you like to add the even or odd of the numbers? ")
+
+def is_even(x):
+    return x % 2
 
 def fib():
     a = 0
     b = 1
     x = a + b
     total = 0
-    for i in range(end_num)
-        while a == 0 or x < end_num:
-            a = b
-            b = x
-            x = a + b
-            if even_odd == "even":
-                total = total + x
-            elif even_odd == "odd":
-                total = total + x
-            elif even_odd == "all":
-                total = total + x
-            else:
-                print("ERROR")
-        print(total)
+    for i in range(num_fib):
+        a = b
+        b = x
+        x = a + b
+        is_even(x)
+        if is_even and even_odd == "even" or (is_even and even_odd == "odd"):
+            total = total + x
+    print(total)
 
 fib()
 
 
-def is_even(x):
-    return x % 2
-
-for i in range ():
-    if(ia_even(i))
-    print(i)
+#def is_even(x):
+#    return x % 2
+#
+#for i in range ():
+#    if(i a_even(i))
+#    print(i)
